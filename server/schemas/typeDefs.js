78c5d6fd98b121b528de37d41cf9b7e54bf192ef
pabfam <pabalate@Pabalates-MBP.attlocal.net>
2023-04-01 11:54:37 -0700

    add code index resolvers typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,39 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: Int
+    savedBooks: [Book]!
+}
+type Book {
+    authors: [String]
+    description: String
+    bookId: ID
+    image: String
+    link: String
+    title: String
+},
+type Auth {
+    token: ID!
+    user: User
+}
+type Query {
+    me: User
+  }
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    loginUser(email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String,
+        bookId: ID,
+        image: String,
+        link: String,
+        title: String): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs
